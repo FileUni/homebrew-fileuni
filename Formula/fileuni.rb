8e6fd493bef9d5fc1acdf6d77c3e6f8b7c0c91cd
FileUni <git@fileuni.com>
2026-03-16 10:06:09 +0000

    chore(release): update formula for 0.0.4-alpha.4.20260316172913

--- Formula/fileuni.rb
+++ Formula/fileuni.rb
@@ -1,26 +1,26 @@
 class Fileuni < Formula
   desc "FileUni CLI"
   homepage "https://fileuni.com"
-  version "0.0.4-alpha.3.20260316152346"
+  version "0.0.4-alpha.4.20260316172913"
   license "Proprietary"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/FileUni/FileUni-Project/releases/download/FileUni-v0.0.4-alpha.3_20260316152346/FileUni-cli-aarch64-macos-darwin.zip"
-      sha256 "0dcae593c364f3324295e57faf267ed13f62d2d52169ddae22897e79877516ef"
+      url "https://github.com/FileUni/FileUni-Project/releases/download/FileUni-v0.0.4-alpha.4_20260316172913/FileUni-cli-aarch64-macos-darwin.zip"
+      sha256 "201f8e98147eb6a3d47d7ac9834af173ffdaf64c9af667a0a3ffb9d7af2d6ac3"
     else
-      url "https://github.com/FileUni/FileUni-Project/releases/download/FileUni-v0.0.4-alpha.3_20260316152346/FileUni-cli-x86_64-macos-darwin.zip"
-      sha256 "24d465d8fe9461f75cbac071d9da2ffee10d10a71728db37b6b3fa630cecf1cb"
+      url "https://github.com/FileUni/FileUni-Project/releases/download/FileUni-v0.0.4-alpha.4_20260316172913/FileUni-cli-x86_64-macos-darwin.zip"
+      sha256 "062fb09e18f3553dfd40b51a4babdcc752e28b2f4fdfd08ce37afeeef3d15590"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/FileUni/FileUni-Project/releases/download/FileUni-v0.0.4-alpha.3_20260316152346/FileUni-cli-aarch64-linux-gnu.zip"
-      sha256 "4355c5b550c43c336476e148cc0012d225668425960c68d9cb8e19fa9bdf60e7"
+      url "https://github.com/FileUni/FileUni-Project/releases/download/FileUni-v0.0.4-alpha.4_20260316172913/FileUni-cli-aarch64-linux-gnu.zip"
+      sha256 "651103d9fdad85ddde555ae34c656e26d5ec9c875bc401bc3f454627374eb07c"
     else
-      url "https://github.com/FileUni/FileUni-Project/releases/download/FileUni-v0.0.4-alpha.3_20260316152346/FileUni-cli-x86_64-linux-gnu.zip"
-      sha256 "101299954b75b97ef8bceb8f4e3dab3955430b5f661d3dfbdc5b3cec89022f0a"
+      url "https://github.com/FileUni/FileUni-Project/releases/download/FileUni-v0.0.4-alpha.4_20260316172913/FileUni-cli-x86_64-linux-gnu.zip"
+      sha256 "c0542fd089d4a913e5681b1a9cd95badffc72c2164e1a5552817db4aa537f572"
     end
   end
 
