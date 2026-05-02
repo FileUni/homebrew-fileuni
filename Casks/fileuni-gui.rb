e63f3974ef2b7f9e752a65b71c9ca44fb9a7dc6f
FileUni <git@fileuni.com>
2026-05-02 05:45:38 +0000

    chore(release): update cli and gui packages for 0.1.13

--- Casks/fileuni-gui.rb
+++ Casks/fileuni-gui.rb
@@ -1,10 +1,10 @@
 cask "fileuni-gui" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.1.12"
-  sha256 arm: "6abe25c7fc8cf51b379bc942fd88a75c60753da2acffc44346635adf1e3538a7", intel: "416a42a037bd0e0cf3e455d19497a4d76e51ac55ca54bd964b4a56d37681a9bf"
+  version "0.1.13"
+  sha256 arm: "83382160270aff546fd54f9de8d33f206289659072e9296cb005be79c92b83ef", intel: "f792e0a2727eb5919616d797f813b56df364d267cb97e3a4f52f56affc400760"
 
-  url "https://github.com/FileUni/FileUni-Project/releases/download/FileUni-v0.1.12/FileUni-gui-#{arch}-macos-darwin.dmg"
+  url "https://github.com/FileUni/FileUni-Project/releases/download/FileUni-v0.1.13/FileUni-gui-#{arch}-macos-darwin.dmg"
   name "FileUni UI"
   desc "FileUni GUI"
   homepage "https://fileuni.com"
--- Formula/fileuni.rb
+++ Formula/fileuni.rb
@@ -1,26 +1,26 @@
 class Fileuni < Formula
   desc "FileUni CLI"
   homepage "https://fileuni.com"
-  version "0.1.12"
+  version "0.1.13"
   license "Proprietary"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/FileUni/FileUni-Project/releases/download/FileUni-v0.1.12/FileUni-cli-aarch64-macos-darwin.zip"
-      sha256 "cc5f85d91cad6e0bbb75dd726c2796677b342e040eaab380cc0c24a621600e83"
+      url "https://github.com/FileUni/FileUni-Project/releases/download/FileUni-v0.1.13/FileUni-cli-aarch64-macos-darwin.zip"
+      sha256 "9b99329b86962282cbca5565b2bcb232409f723d1e8d55071f33332b74c6906d"
     else
-      url "https://github.com/FileUni/FileUni-Project/releases/download/FileUni-v0.1.12/FileUni-cli-x86_64-macos-darwin.zip"
-      sha256 "f2858d4a27c454fcf05201a962938bd55adba6540f1309cc25cd8e7d99ee5b12"
+      url "https://github.com/FileUni/FileUni-Project/releases/download/FileUni-v0.1.13/FileUni-cli-x86_64-macos-darwin.zip"
+      sha256 "c00265fd2ef2339c37596721038dc7ce661f31fa1680ca002477258499b8ee00"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/FileUni/FileUni-Project/releases/download/FileUni-v0.1.12/FileUni-cli-aarch64-linux-gnu.zip"
-      sha256 "d90bf48f342e68e73623dbeab8a5b430d8f037deb790a168f3aae2d50d67a591"
+      url "https://github.com/FileUni/FileUni-Project/releases/download/FileUni-v0.1.13/FileUni-cli-aarch64-linux-gnu.zip"
+      sha256 "3b2160ab16a6239dc9c61d6f4927cf052e4b9aadca5dd33c896a16f960d2ab30"
     else
-      url "https://github.com/FileUni/FileUni-Project/releases/download/FileUni-v0.1.12/FileUni-cli-x86_64-linux-gnu.zip"
-      sha256 "ab7fb55e58ee13af117fcabdf9beea1ca182fa56cd8252938a7c39fb5ebd4345"
+      url "https://github.com/FileUni/FileUni-Project/releases/download/FileUni-v0.1.13/FileUni-cli-x86_64-linux-gnu.zip"
+      sha256 "fa1dd0e74cd82ac08471afa1811299fd914300458fee9d4c2c0f2b747a08e3f5"
     end
   end
 
